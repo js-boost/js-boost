c6c2df21ac09dbcd7aed51f2ae3a3037519ae66d
Marcell Kiss <kissmarcell88@gmail.com>
2023-10-27 16:14:07 +0200

    feat: add tests

--- packages/xlf-translator/src/lib/xlf-translator.spec.ts
+++ packages/xlf-translator/src/lib/xlf-translator.spec.ts
@@ -1,6 +1,23 @@
+import * as fs from 'fs';
+import * as util from 'util';
+import { Locale, XlfTranslator } from './xlf-translator';
+
 describe('translateXlf', () => {
-  it('should work', () => {
-    // expect(XlfTranslator()).toEqual('xlf-translator');
-    expect(true).toBeTruthy();
+  it('locale file should be created', async () => {
+    const localeFile = {
+      locale: 'de-DE' as Locale,
+      filePath: 'packages/xlf-translator/src/lib/test.de.xlf',
+    };
+
+    const translator = new XlfTranslator(
+      'packages/xlf-translator/src/lib/test.xlf',
+      [localeFile],
+      () => Promise.resolve('Translation Result Sample')
+    );
+    const fileString = await translator
+      .start()
+      .then(() => util.promisify(fs.readFile)(localeFile.filePath, 'utf-8'));
+
+    expect(fileString).toContain('Translation Result Sample');
   });
 });
--- packages/xlf-translator/src/lib/xlf-translator.ts
+++ packages/xlf-translator/src/lib/xlf-translator.ts
@@ -1,4 +1,4 @@
-import chalk from 'chalk';
+import * as chalk from 'chalk';
 import * as fs from 'fs';
 import * as util from 'util';
 import * as xml2js from 'xml2js';
@@ -20,68 +20,86 @@ export class XlfTranslator {
     console.log(`localeFiles:`, localeFiles);
   }
 
-  async start() {
+  async start(): Promise<void> {
     try {
       // Step 1: Create termsMap file
       const originalTermsMap = await this.getTermsMap();
 
       // Step 2: Loop through locales and update translation files
-      this.localeFiles.forEach(async ({ locale, filePath }) => {
-        const termsMap = new Map(originalTermsMap);
+      const localeExecutions$ = this.localeFiles.map(
+        async ({ locale, filePath }) => {
+          const termsMap = new Map(originalTermsMap);
 
-        // Copy-paste file if it doesn't exist
-        try {
-          await this.fileExists$(filePath, fs.constants.R_OK);
-        } catch (error) {
-          await this.copyFile$(this.inputXlfFilePath, filePath);
-        }
-
-        const xmlJson = await this.getXmlJsonByPath(filePath);
-        const transUnits = xmlJson.xliff.file[0].body[0]['trans-unit'];
-        const localeTermsMap = new Map(
-          transUnits.map((transUnit: any) => [transUnit.$.id, transUnit])
-        );
-
-        console.log(`There are ${transUnits.length} terms for ${locale}`);
-        let counter = 0;
-        const transUnits$ = Array.from(termsMap).map(
-          async ([id, transUnit]) => {
-            let newTransUnit: any = localeTermsMap.get(id);
-
-            if (!newTransUnit || !newTransUnit.target) {
-              // Do translation
-              newTransUnit = transUnit;
-              let sourceText = newTransUnit.source[0];
-              const translationText = await this.translate(sourceText, locale);
-              newTransUnit.target = [translationText];
-
-              console.log(
-                chalk.yellow(
-                  `Translated [${locale}], '${sourceText}' to '${translationText}' | id: ${transUnit.$.id}`
-                )
-              );
-            }
-
-            if (++counter % 50 === 0) {
-              console.log(
-                `Processed ${chalk.green(`${counter} terms`)} for ${locale}`
-              );
-            }
-
-            return newTransUnit;
+          // Copy-paste file if it doesn't exist
+          try {
+            await this.fileExists$(filePath, fs.constants.R_OK);
+          } catch (error) {
+            await this.copyFile$(this.inputXlfFilePath, filePath);
           }
-        );
 
-        xmlJson.xliff.file[0].body[0]['trans-unit'] = await Promise.all(
-          transUnits$
-        );
+          const xmlJson = await this.getXmlJsonByPath(filePath);
+          const transUnits = xmlJson.xliff.file[0].body[0]['trans-unit'];
+          console.log(`There are ${transUnits.length} terms for ${locale}`);
 
-        // Convert the JavaScript object back to XML
-        this.saveFile(xmlJson, filePath);
-      });
+          const localeTermsMap = new Map<number, string>(
+            transUnits.map((transUnit: any) => [transUnit.$.id, transUnit])
+          );
+          const newTransUnits$ = this.getNewTransUnits$(
+            termsMap,
+            localeTermsMap,
+            locale
+          );
+
+          xmlJson.xliff.file[0].body[0]['trans-unit'] = await Promise.all(
+            newTransUnits$
+          );
+
+          // Convert the JavaScript object back to XML
+          await this.saveFile(xmlJson, filePath);
+
+          return;
+        }
+      );
+
+      await Promise.all(localeExecutions$);
+
+      return;
     } catch (error) {
       console.error(error);
     }
+  }
+
+  private getNewTransUnits$(
+    termsMap: Map<number, any>,
+    localeTermsMap: Map<number, any>,
+    locale: Locale
+  ): Promise<any>[] {
+    let counter = 0;
+    return Array.from(termsMap).map(async ([id, transUnit]) => {
+      let newTransUnit: any = localeTermsMap.get(id);
+
+      if (!newTransUnit || !newTransUnit.target) {
+        // Do translation
+        newTransUnit = transUnit;
+        let sourceText = newTransUnit.source[0];
+        const translationText = await this.translate(sourceText, locale);
+        newTransUnit.target = [translationText];
+
+        console.log(
+          chalk.yellow(
+            `Translated [${locale}], '${sourceText}' to '${translationText}' | id: ${transUnit.$.id}`
+          )
+        );
+      }
+
+      if (++counter % 50 === 0) {
+        console.log(
+          `Processed ${chalk.green(`${counter} terms`)} for ${locale}`
+        );
+      }
+
+      return newTransUnit;
+    });
   }
 
   private async getTermsMap() {
